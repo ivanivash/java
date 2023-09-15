e19dc8f1b08b10aae3344985f26122822f7f12e7
Іван Івашинюта <ivanivash@ukr.net>
2023-09-15 20:51:04 +0300

    authPage

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,25 @@
 import './App.css'
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import DefaultLayout from "./components/containers/default/DefaultLayout.tsx";
 import CategoryListPage from "./components/category/list/CategoryListPage.tsx";
 import CategoryCreatePage from './components/category/create/CategoryCreatePage.tsx';
 import ProductCreatePage from './components/product/creat/ProductCreatePage.tsx';
-
-
-
+import LoginPage from './components/auth/login/LoginPage.tsx';
 
 function App() {
-    
 
   return (
     <>
-        <Routes>
-            <Route path={"/"} element={<DefaultLayout/>}>
-                <Route index element={<CategoryListPage />} />
-                <Route path={"create"} element={<CategoryCreatePage />} />
-                <Route path={"product"}>
-                    <Route path={"create"} element={<ProductCreatePage/>} />
-                </Route>
-            </Route>
-        </Routes>
+      <Routes>
+        <Route path={"/"} element={<DefaultLayout />}>
+          <Route index element={<CategoryListPage />} />
+          <Route path={"create"} element={<CategoryCreatePage />} />
+          <Route path={"product"}>
+            <Route path={"create"} element={<ProductCreatePage />} />
+          </Route>
+          <Route path={"login"} element={<LoginPage />}></Route>
+        </Route>
+      </Routes>
     </>
   )
 }
